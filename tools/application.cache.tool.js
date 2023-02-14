bbc918408c78e4852155b48499dd917bbc88fe91
Yogesh P. Murdeshwar <yogesh@chekhub.com>
2023-02-14 17:11:44 +0530

    adding cache

--- tools/application.cache.tool.js
+++ tools/application.cache.tool.js
@@ -1,4 +1,5 @@
 const Mongo = require('../tools/mongo.tool');
+const Logger = require('../tools/Log.tool');
 const DB_COLLECTIONS = require('../contants/db-collections');
 
 const getMicroservices = async () => {
@@ -10,14 +11,32 @@ const getMicroservices = async () => {
     return map;
 };
 
+const getEmailTemplates = async () => {
+    const pipeline = [];
+    const project = {
+        _id: 1,
+        name: 1
+    }
+    pipeline.push({$project: project});
+    const email_templates = await Mongo.aggregate(DB_COLLECTIONS.EMAIL_TEMPLATES, pipeline);
+    const map = {};
+    email_templates.forEach(et => {
+        map[et.name] = et._id;
+    });
+    return map;
+};
+
 class ApplicationCache {
 
     microservices;
+    email_templates;
 
     constructor() {}
 
     async init() {
         this.microservices = await getMicroservices();
+        this.email_templates = await getEmailTemplates();
+        Logger.info('Initialized application cache...')
     }
 
     
